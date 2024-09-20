fba0009b3262cc55ec65b3dc62969e49132682b7
khh-Niloy <niloy.dev.101@gmail.com>
2024-09-20 15:05:02 +0600

    name and pass function

--- project.c
+++ project.c
@@ -1,14 +1,89 @@
 #include<stdio.h>
 #include<string.h>
+#include <ctype.h>
+
+
 
 int main () {
 
-    char task[100][100];
-    int taskCount= 0;
+    /* char task[100][100];
+    int taskCount= 0; */
 
-    printf("\nTo-Do List Application:\n");
+
+    printf("\n\n");
+    for ( int i = 0; i<20; i++ ) {
+        if ( i == 10 ) {
+            printf("Log in");
+        }
+        else {
+            printf("-");
+        }
+    }
+    printf("\n\n");
+
+    printf("Enter Username: ");
+    char username[20];
+    gets(username);
+
+    printf("Enter password: (maximum 8 char, must have one character and one digit and one uppercase letter)\n");
+
+    int flag = -1;
+    while ( flag != 0 ) {
+
+        int aplha = 0;
+        int digit = 0;
+        int uppercase = 0;
+
+        char pass[100];
+        gets(pass);
+
+        for ( int i = 0; i<9; i++ ) {
+            if ( isalpha(pass[i]) && isupper(pass[i])) {
+                aplha++;
+                uppercase++;
+            }
+            else if ( isdigit(pass[i]) ) {
+                digit++;
+            }
+        }
+
+        if ( aplha > 0 && digit > 0 && uppercase > 0 && strlen(pass) >=8 ) {
+            flag = 0;
+            printf("\nUsername -> ");
+            puts(username);
+
+            printf("password -> ");
+            for ( int i = 0; i<strlen(pass); i++ ) {
+                printf("*");
+            }
+
+            printf("\n\nwrite y or Y to see your password\n");
+            char ch;
+            scanf("%c", &ch);
+            if ( ch == 'Y' || ch == 'y' ) {
+                printf("\nUsername -> ");
+                puts(username);
+
+                printf("password -> ");
+                puts(pass);
+            }
+        }
+        else {
+            printf("failed to meet the criteria\n");
+            flag = -1;
+        }
+    }
+
+
+    // puts(pass);
+
+
+
+
+    /* printf("\nTo-Do List Application:\n");
     printf("1. View all tasks\n");
     printf("2. Add a new task\n");
+    printf("4. Mark task as completed\n");
     printf("3. Exit\n");
     printf("Enter your choice: ");
 
@@ -28,6 +103,8 @@ int main () {
             }
             printf("Successfully added %d task\n", taskCount);
             printf("to add task press 2\n");
+            printf("4. Mark task as completed\n");
+
             printf("press 3 for exit\n");
 
         }
@@ -46,9 +123,41 @@ int main () {
         printf("\nadded task -> %s\n\n", task[taskCount]);
         taskCount++;
         printf("to show list press 1\nto add task press 2\n");
+        printf("4. Mark task as completed\n");
         printf("press 3 for exit\n");
     }
+
+    else if ( choice == 4 ) {
+        int completedTask;
+
+        for ( int i = 0; i<taskCount; i++ ) {
+            if ( completedTask-1 == i ) {
+                printf("%d. %s (completed)\n",i+1, task[i]);
+            }
+            else{
+                printf("%d. %s\n",i+1, task[i]);
+            }
+        }
+
+        printf("\nWhich number you completed?");
+        scanf("%d", &completedTask);
+
+        for ( int i = 0; i<taskCount; i++ ) {
+            if ( completedTask-1 == i ) {
+                printf("%d. %s (completed)\n",i+1, task[i]);
+            }
+            else{
+                printf("%d. %s\n",i+1, task[i]);
+            }
+        }
+
+        printf("to add task press 2\n");
+        printf("press 4 to Mark another task as completed\n");
     }
+
+
+    } */ //while
+
     
    
    return 0;
