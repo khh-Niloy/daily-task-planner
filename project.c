2a712486b8a99798cc81728416958482b245d6e1
khh-Niloy <niloy.dev.101@gmail.com>
2024-09-20 16:01:46 +0600

    implemented completed task function

--- project.c
+++ project.c
@@ -2,15 +2,112 @@
 #include<string.h>
 #include <ctype.h>
 
+    // global declaration
+    char task[100][100];
+    char completedTaskList[100];
+    int taskCount= 0;
 
+
+void viewTask(){
+
+    if ( taskCount == 0 ) {
+            printf("No task added yet! Press 2 for adding task\n");
+        }
+        else {
+
+            printf("\n\n");
+            for ( int i = 0; i<20; i++ ) {
+                if ( i == 10 ) {
+                    printf("All task list");
+                }
+                else {
+                    printf("-");
+                }
+            }
+            printf("\n\n");
+
+            for ( int i = 0; i<taskCount; i++ ) {
+                if ( completedTaskList[i] == 1 ) {
+                    printf("%d. %s(completed)\n",i+1, task[i]);
+                }
+                else {
+                    printf("%d. %s\n",i+1, task[i]);
+                }
+            }
+
+            printf("\n\n");
+            for ( int i = 0; i<20; i++ ) {
+                printf("-");
+            }
+            printf("\n\n");
+
+            printf("to add task press 2\n");
+            printf("4. Mark task as completed\n");
+
+            printf("press 3 for exit\n");
+
+        }
+}
+
+
+
+
+void addTask(){
+    printf("write a new task: ");
+    char ch[100];
+	fgets(ch, 100, stdin);
+    int length = strlen(ch);
+
+    for ( int j = 0; j<length; j++ ) {
+        task[taskCount][j] = ch[j];
+    }
+    task[taskCount][length] = '\0';
+    printf("\nlast added task -> %s\n\n", task[taskCount]);
+    taskCount++;
+    printf("to show list press 1\nto add task press 2\n");
+    printf("4. Mark task as completed\n");
+    printf("press 3 for exit\n");
+}
+
+
+
+
+
+void markCompletedTask(){
+    int completedTask;
+
+    if ( taskCount == 0 ) {
+        printf("No task added yet! Press 2 for adding task\n");
+    }
+
+    else{
+        printf("\nWhich number you completed? ");
+        scanf("%d", &completedTask);
+        completedTaskList[completedTask-1] = 1;
+
+        for ( int i = 0; i<taskCount; i++ ) {
+            if ( completedTask == i+1 ) {
+                printf("%d. %s (this marked as completed)\n",i+1, task[i]);
+            }
+            else{
+                printf("%d. %s\n",i+1, task[i]);
+            }
+        }
+
+        printf("\n\n\n");
+        printf("to show list press 1\nto add task press 2\n");
+        printf("press 3 for exit\n");    
+    }
+    
+
+}
 
 int main () {
 
-    /* char task[100][100];
-    int taskCount= 0; */
 
 
-    printf("\n\n");
+    // username and pass
+    /* printf("\n\n");
     for ( int i = 0; i<20; i++ ) {
         if ( i == 10 ) {
             printf("Log in");
@@ -72,91 +169,35 @@ int main () {
             printf("failed to meet the criteria\n");
             flag = -1;
         }
-    }
-
-
-    // puts(pass);
+    } */
 
 
 
-
-    /* printf("\nTo-Do List Application:\n");
+    printf("\nTo-Do List Application:\n");
     printf("1. View all tasks\n");
     printf("2. Add a new task\n");
     printf("4. Mark task as completed\n");
-    printf("3. Exit\n");
+    printf("3. Exit\n\n");
     printf("Enter your choice: ");
 
     int choice = 0;
     while ( choice!=3 ) {
-    scanf("%d", &choice);
-    getchar();
+        scanf("%d", &choice);
+        getchar();
 
-    if ( choice == 1 ) {
-
-        if ( taskCount == 0 ) {
-            printf("No task added yet! Press 2 for adding task\n");
-        }
-        else {
-            for ( int i = 0; i<taskCount; i++ ) {
-                printf("%d. %s\n",i+1, task[i]);
-            }
-            printf("Successfully added %d task\n", taskCount);
-            printf("to add task press 2\n");
-            printf("4. Mark task as completed\n");
-
-            printf("press 3 for exit\n");
-
-        }
-    }
-
-    else if ( choice == 2 ) {
-        printf("write a new task: ");
-        char ch[100];
-	    fgets(ch, 100, stdin);
-        int length = strlen(ch);
-
-        for ( int j = 0; j<length; j++ ) {
-            task[taskCount][j] = ch[j];
-        }
-        task[taskCount][length] = '\0';
-        printf("\nadded task -> %s\n\n", task[taskCount]);
-        taskCount++;
-        printf("to show list press 1\nto add task press 2\n");
-        printf("4. Mark task as completed\n");
-        printf("press 3 for exit\n");
-    }
-
-    else if ( choice == 4 ) {
-        int completedTask;
-
-        for ( int i = 0; i<taskCount; i++ ) {
-            if ( completedTask-1 == i ) {
-                printf("%d. %s (completed)\n",i+1, task[i]);
-            }
-            else{
-                printf("%d. %s\n",i+1, task[i]);
-            }
+        if ( choice == 1 ) {
+            viewTask();
         }
 
-        printf("\nWhich number you completed?");
-        scanf("%d", &completedTask);
-
-        for ( int i = 0; i<taskCount; i++ ) {
-            if ( completedTask-1 == i ) {
-                printf("%d. %s (completed)\n",i+1, task[i]);
-            }
-            else{
-                printf("%d. %s\n",i+1, task[i]);
-            }
+        else if ( choice == 2 ) {
+            addTask();
         }
 
-        printf("to add task press 2\n");
-        printf("press 4 to Mark another task as completed\n");
-    }
+        else if ( choice == 4 ) {
+            markCompletedTask();
+        }
 
-
-    } */ //while
+    } //while
 
     
    
